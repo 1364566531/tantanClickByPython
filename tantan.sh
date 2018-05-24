b72fa76881c90b9e33b6b20115de1c5644241d76
何悦 <vipheyue@foxmail.com>
2018-05-24 22:48:50 +0800

    Update tantan.sh

--- tantan.sh
+++ tantan.sh
@@ -3,6 +3,8 @@ while true
 do
     adb shell input swipe 250 600 700 600
     sleep 1 
+    adb shell input swipe 250 600 700 600
+    sleep 1 
     adb shell input swipe 400 600 250 600 
     sleep 1 
 done
